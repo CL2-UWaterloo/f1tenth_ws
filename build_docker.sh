1c48fe99ae2a6c98060fcf70e19bf61535d7f418
Hongrui (Billy) Zheng <hzheng40@users.noreply.github.com>
2020-04-22 12:21:13 -0400

    Update build_docker.sh

--- build_docker.sh
+++ build_docker.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+if [ ! -d f1tenth_gym ] ; then
+    git clone https://github.com/f1tenth/f1tenth_gym
+else
+    echo f1tenth_gym exists, not cloning.
+fi
 docker build -t f1tenth_gym -f Dockerfile .
