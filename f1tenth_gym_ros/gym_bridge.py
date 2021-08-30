760feb7cb1f46314cfa78d6f6795c7eec905e6b4
billyzheng <billyzheng.bz@gmail.com>
2021-08-30 11:15:12 -0400

    update

--- f1tenth_gym_ros/gym_bridge.py
+++ f1tenth_gym_ros/gym_bridge.py
@@ -30,6 +30,7 @@ from geometry_msgs.msg import TransformStamped
 from geometry_msgs.msg import Transform
 from geometry_msgs.msg import Quaternion
 from ackermann_msgs.msg import AckermannDriveStamped
+from tf2_ros import TransformBroadcaster
 
 import gym
 
@@ -37,11 +38,18 @@ class GymBridge(Node):
     def __init__(self):
         super().__init__('gym_bridge')
 
+        # check num_agents
+        num_agents = self.get_parameter('num_agent').get_parameter_value()
+        if num_agents < 1 or num_agents > 2:
+            raise ValueError('num_agents should be either 1 or 2.')
+        elif type(num_agents) != int:
+            raise ValueError('num_agents should be an int.')
+
         # env backend
         self.env = gym.make('f110_gym:f110_v0',
             map=self.get_parameter('map_path').get_parameter_value(),
             map_ext=self.get_parameter('map_img_ext').get_parameter_value(),
-            num_agents=self.get_parameter('num_agent').get_parameter_value())
+            num_agents=num_agents)
 
         # params
         ego_scan_topic = self.get_parameter('ego_scan_topic').get_parameter_value().string_value
@@ -50,13 +58,26 @@ class GymBridge(Node):
         opp_scan_topic = self.get_parameter('opp_scan_topic').get_parameter_value().string_value
         opp_odom_topic = self.get_parameter('opp_odom_topic').get_parameter_value().string_value
         opp_drive_topic = self.get_parameter('opp_drive_topic').get_parameter_value().string_value
+        scan_fov = self.get_parameter('scan_fov').get_parameter_value()
+        scan_beams = self.get_parameter('scan_beams').get_parameter_value()
+        self.angle_min = -scan_fov / 2.
+        self.angle_max = scan_fov / 2.
+        self.angle_inc = scan_fov / scan_beams
 
         # sim physical step timer
-        self.timer = self.create_timer(0.01, self.timer_callback)
+        self.drive_timer = self.create_timer(0.01, self.drive_timer_callback)
+        # topic publishing timer
+        self.timer = self.create_timer(0.004, self.timer_callback)
+
+        # transform broadcaster
+        self.br = TransformBroadcaster(self)
 
         # publishers
         self.ego_scan_pub = self.create_publisher(LaserScan, ego_scan_topic, 10)
         self.ego_odom_pub = self.create_publisher(Odometry, ego_odom_topic, 10)
+        self.opp_odom_pub = self.create_publisher(Odometry, opp_odom_topic, 10)
+        if num_agents == 2:
+            self.opp_scan_pub = self.create_publisher(LaserScan, opp_scan_topic, 10)
 
         # subscribers
         self.ego_drive_sub = self.create_subscription(
@@ -64,15 +85,83 @@ class GymBridge(Node):
             ego_drive_topic,
             self.drive_callback,
             10)
+        if num_agents == 2:
+            self.has_opp = True
+            self.opp_drive_sub = self.create_subscription(
+                AckermannDriveStamped,
+                opp_drive_topic,
+                self.opp_drive_callback,
+                10)
 
     def drive_callback(self, drive_msg):
+        self.ego_requested_speed = drive_msg.drive.speed
+        self.ego_steer = drive_msg.drive.steering_angle
+        self.ego_drive_published = True
+
+    def opp_drive_callback(self, drive_msg):
         pass
 
     def pose_callback(self, pose_msg):
         pass
 
+    def drive_timer_callback(self):
+        if self.ego_drive_published and self.opp_drive_published:
+            self.obs, _, self.done, _ = self.env.step(np.array([[self.ego_steer, self.ego_speed], [self.opp_steer, self.opp_speed]]))
+        self._update_sim_state()
+
     def timer_callback(self):
-        pass
+        ts = self.get_clock().now().to_msg()
+
+        # pub scans
+        scan = LaserScan()
+        scan.header.stamp = ts
+        scan.header.frame_id = '/laser'
+        scan.angle_min = self.angle_min
+        scan.angle_max = self.angle_max
+        scan.angle_increment = self.angle_inc
+        scan.range_min = 0.
+        scan.range_max = 30.
+        scan.ranges = self.ego_scan
+        self.ego_scan_pub.publish(scan)
+
+        if self.has_opp:
+            opp_scan = LaserScan()
+            opp_scan.header.stamp = ts
+            opp_scan.header.frame_id = 'opp_racecar/laser'
+            opp_scan.angle_min = self.angle_min
+            opp_scan.angle_max = self.angle_max
+            opp_scan.angle_increment = self.angle_inc
+            opp_scan.range_min = 0.
+            opp_scan.range_max = 30.
+            opp_scan.ranges = self.opp_scan
+            self.opp_scan_pub.publish(opp_scan)
+
+        # pub tf
+        self.publish_odom(ts)
+        self.publish_transforms(ts)
+        self.publish_laser_transforms(ts)
+        self.publish_wheel_transforms(ts)
+
+        # TODO: pub race info?
+
+    def _update_sim_state(self):
+        self.ego_scan = list(self.obs['scans'][0])
+        if self.has_opp:
+            self.opp_scan = list(self.obs['scans'][1])
+
+        self.ego_pose[0] = self.obs['poses_x'][0]
+        self.ego_pose[1] = self.obs['poses_y'][0]
+        self.ego_pose[2] = self.obs['poses_theta'][0]
+        self.ego_speed[0] = self.obs['linear_vels_x'][0]
+        self.ego_speed[1] = self.obs['linear_vels_y'][0]
+        self.ego_speed[2] = self.obs['ang_vels_z'][0]
+
+        self.opp_pose[0] = self.obs['poses_x'][1]
+        self.opp_pose[1] = self.obs['poses_y'][1]
+        self.opp_pose[2] = self.obs['poses_theta'][1]
+        self.opp_speed[0] = self.obs['linear_vels_x'][1]
+        self.opp_speed[1] = self.obs['linear_vels_y'][1]
+        self.opp_speed[2] = self.obs['ang_vels_z'][1]
 
 def main(args=None):
     rclpy.init(args=args)
